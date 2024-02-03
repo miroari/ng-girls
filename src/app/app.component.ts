f6ce01ac70e000180cf42bac8ca18f400da0f89d
miroari <arina.mir@gmail.com>
2024-02-03 11:16:00 +0100

    create project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {CommonModule} from "@angular/common";
+import {InputButtonUnitComponent} from "./input-button-unit/input-button-unit.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, InputButtonUnitComponent],
   template: `
   <h1>
     Welcome to {{ title }}!
   </h1>
+
+  <app-input-button-unit></app-input-button-unit>
 `,
   styleUrl: './app.component.scss'
 })
--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {CommonModule} from "@angular/common";
+import {AppComponent} from "../app.component";
 
 @Component({
   selector: 'app-input-button-unit',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AppComponent],
   templateUrl: './input-button-unit.component.html',
   styleUrl: './input-button-unit.component.scss'
 })
